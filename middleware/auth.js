7ce2ced8c01c2d77584c3be740d7f2f226b6ed86
kenil09 <keniltogadiya100@gmail.com>
2023-03-24 10:45:32 +0530

    fix auth token validation

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,7 @@ const verifyToken = async (req, res, next) => {
     token = token.replace("Bearer ", "");
 
     jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, data) => {
-      if (err) res.status(403).json({ message: "Invalid token" });
+      if (err) return res.status(403).json({ message: "Invalid token" });
       const user = await User.findOne({ _id: data._id })
         .populate("company")
         .populate("department")
